ff8daf6903710cd1683c76b6683418f6e8bb06a2
Narayandas Kachhi <126740162+narayan24x7@users.noreply.github.com>
2025-08-24 02:43:10 +0000

    Initial commit

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -2,7 +2,6 @@ import { Navigation } from "./Navigation";
 import { Hero } from "./Hero";
 import { About } from "./About";
 import { Skills } from "./Skills";
-import { Services } from "./Services";
 import { Projects } from "./Projects";
 import { Experience } from "./Experience";
 import { Contact } from "./Contact";
